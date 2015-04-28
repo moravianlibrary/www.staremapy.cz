a231f6f3b5265549b4f93cb46f05e4017cec69f6
Erich Duda <dudaerich@gmail.com>
2015-04-28 16:01:16 +0200

    Styles moved to css file.

--- georeferencer/umisti/main.js
+++ georeferencer/umisti/main.js
@@ -15,22 +15,13 @@ georeferencer.umisti.loadCss = function(url) {
 
 georeferencer.umisti.main = function() {
   georeferencer.umisti.loadCss('http://staremapy.cz/css/fonts.css');
+  georeferencer.umisti.loadCss('http://staremapy.cz/georeferencer/umisti/main.css');
 
   var zoomPanel = document.querySelector('#main-left .olControlZoomPanel');
   goog.asserts.assertObject(zoomPanel);
 
   var addPointBttn = goog.dom.createElement('div');
-  addPointBttn.style.width = '30px';
-  addPointBttn.style.height = '30px';
-  addPointBttn.style.position = 'absolute';
-  addPointBttn.style.left = '0';
-  addPointBttn.style.top = '64px';
-  addPointBttn.style.fontSize = '25px';
-  addPointBttn.style.color = 'white';
-  addPointBttn.style.backgroundColor = 'rgb(145, 143, 134)';
-  addPointBttn.style.textAlign = 'center';
-  addPointBttn.style.lineHeight = '30px';
-  addPointBttn.style.borderRadius = '4px';
+  addPointBttn.id = 'addpoint';
   goog.dom.classes.add(addPointBttn, 'icon-map-marker');
 
   goog.dom.append(zoomPanel, addPointBttn);
