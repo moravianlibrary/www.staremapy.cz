5a5ec1e2e630ec8c0d1429ec44bf085143d3def5
Erich Duda <dudaerich@gmail.com>
2015-06-25 17:26:21 +0200

    Modyfing of review interface

--- georeferencer/review.js
+++ georeferencer/review.js
@@ -1,3 +1,6 @@
+var georeferencer = georeferencer || {};
+var review = review || {};
+
 var georeferencer.review.labelMap = function(id, value) {
   alert(id + ': ' + value);
 };
@@ -13,7 +16,7 @@ var georeferencer.review.main = function() {
   var form = document.getElementById('review-form');
   var buttons = form.childNodes[0];
   var id = georef.name + '/' + georef.version;
-  
+
   buttons.appendChild(georeferencer.review.createButton('Více map', id, 'vicemap'));
   buttons.appendChild(georeferencer.review.createButton('Rozřezaná', id, 'rozrezana'));
   buttons.appendChild(georeferencer.review.createButton('Nelze umístit', id, 'nelzeumistit'));
