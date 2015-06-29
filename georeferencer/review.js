bc9b3f513a60c23e1081b2aa6b7ef2697db44a39
Erich Duda <dudaerich@gmail.com>
2015-06-29 10:10:51 +0200

    Developing review

--- georeferencer/review.js
+++ georeferencer/review.js
@@ -34,11 +34,41 @@ georeferencer.review.labelMap = function(bttn, author, id, value) {
   img.src = 'http://www.staremapy.cz/img/ajax-loader-mini.gif';
 };
 
+georeferencer.review.getLabel = function(bttn, id, value) {
+  var img = bttn.getElementsByTagName('img')[0];
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var response = JSON.parse(xmlhttp.responseText);
+      if (response.status == 'ok') {
+        if (reponse.data.value = value) {
+          img.src = 'http://www.staremapy.cz/img/success.png';
+          var form = document.getElementById('review-form');
+          var buttons = form.getElementsByClassName('addLabelBttn');
+          for (var i = 0; i < buttons.length; i++) {
+            var button = buttons[i];
+            if (button != bttn) {
+              button.disabled = true;
+            }
+          }
+        }
+      }
+    }
+  };
+  var idParam = encodeURIComponent(id);
+  var token = 'cab08dc4-e7c6-4ca1-b2ad-393ec198c31d';
+  var requestParams = '?key=' + idParam + '&token=' + token + '&action=get';
+  var url = 'http://195.113.155.123/cgi-bin/addlabeltomap.py' + requestParams;
+  xmlhttp.open('GET', url, true);
+  xmlhttp.send();
+}
+
 georeferencer.review.createButton = function(label, author, id, value) {
   var button = document.createElement('BUTTON');
   button.className = 'addLabelBttn';
   button.innerHTML = label + ' <img>';
   button.onclick = function() { georeferencer.review.labelMap(this, author, id, value); return false; };
+  georeferencer.review.getLabel(button, id, value);
   return button;
 };
 
