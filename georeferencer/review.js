fbd498b30c85ff56338c1a2a83e52d713232779b
Erich Duda <dudaerich@gmail.com>
2015-07-20 13:25:43 +0200

    Review develop

--- georeferencer/review.js
+++ georeferencer/review.js
@@ -12,13 +12,13 @@ georeferencer.review.enableButtons = function(buttons, activeBttn, value) {
 
 georeferencer.review.actionBttn = function(bttn, author, id, value) {
   if (bttn.className == 'active') {
-    georeferencer.review.unlabelMap(bttn, id);
+    georeferencer.review.unlabelMap(bttn, id, institution);
   } else {
-    georeferencer.review.labelMap(bttn, author, id, value);
+    georeferencer.review.labelMap(bttn, author, id, institution, value);
   }
 };
 
-georeferencer.review.labelMap = function(bttn, author, id, value) {
+georeferencer.review.labelMap = function(bttn, author, id, institution, value) {
   var img = bttn.getElementsByTagName('img')[0];
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
@@ -38,16 +38,17 @@ georeferencer.review.labelMap = function(bttn, author, id, value) {
   };
   var authorParam = encodeURIComponent(author);
   var idParam = encodeURIComponent(id);
+  var institutionParam = encodeURIComponent(institution);
   var valueParam = encodeURIComponent(value);
   var token = 'cab08dc4-e7c6-4ca1-b2ad-393ec198c31d';
-  var requestParams = '?author=' + authorParam + '&key=' + idParam + '&value=' + valueParam + '&token=' + token + '&action=put';
+  var requestParams = '?author=' + authorParam + '&key=' + idParam + '&value=' + valueParam + '&institution=' + institution + '&token=' + token + '&action=put';
   var url = 'http://195.113.155.123/cgi-bin/addlabeltomap.py' + requestParams;
   xmlhttp.open('GET', url, true);
   xmlhttp.send();
   img.src = 'http://www.staremapy.cz/img/ajax-loader-mini.gif';
 };
 
-georeferencer.review.unlabelMap = function(bttn, id) {
+georeferencer.review.unlabelMap = function(bttn, id, institution) {
   var img = bttn.getElementsByTagName('img')[0];
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
@@ -66,8 +67,9 @@ georeferencer.review.unlabelMap = function(bttn, id) {
     }
   };
   var idParam = encodeURIComponent(id);
+  var institutionParam = encodeURIComponent(institution);
   var token = 'cab08dc4-e7c6-4ca1-b2ad-393ec198c31d';
-  var requestParams = '?key=' + idParam + '&token=' + token + '&action=delete';
+  var requestParams = '?key=' + idParam + '&institution=' + institution + '&token=' + token + '&action=delete';
   var url = 'http://195.113.155.123/cgi-bin/addlabeltomap.py' + requestParams;
   xmlhttp.open('GET', url, true);
   xmlhttp.send();
@@ -99,10 +101,10 @@ georeferencer.review.getLabel = function(bttn, id, value) {
   xmlhttp.send();
 }
 
-georeferencer.review.createButton = function(label, author, id, value) {
+georeferencer.review.createButton = function(label, author, id, institution, value) {
   var button = document.createElement('BUTTON');
   button.innerHTML = label + ' <img>';
-  button.onclick = function() { georeferencer.review.actionBttn(this, author, id, value); return false; };
+  button.onclick = function() { georeferencer.review.actionBttn(this, author, id, institution, value); return false; };
   georeferencer.review.getLabel(button, id, value);
   return button;
 };
@@ -112,10 +114,11 @@ georeferencer.review.main = function() {
   var buttons = form.getElementsByClassName('buttons')[0];
   var author = document.getElementById('header-userinfo-name').innerHTML;
   var id = /^.*\/map\/([^\/]+\/[^\/]+).*$/.exec(location.href)[1];
+  var institution = /^(\w+)\..*\..*$/.exec(location.hostname)[1];
 
-  buttons.appendChild(georeferencer.review.createButton('Více map', author, id, 'vicemap'));
-  buttons.appendChild(georeferencer.review.createButton('Rozřezaná', author, id, 'rozrezana'));
-  buttons.appendChild(georeferencer.review.createButton('Nelze umístit', author, id, 'nelzeumistit'));
+  buttons.appendChild(georeferencer.review.createButton('Více map', author, id, institution, 'vicemap'));
+  buttons.appendChild(georeferencer.review.createButton('Rozřezaná', author, id, institution, 'rozrezana'));
+  buttons.appendChild(georeferencer.review.createButton('Nelze umístit', author, id, institution, 'nelzeumistit'));
 };
 
 georeferencer.review.main();
