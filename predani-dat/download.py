f6b0a12e479cb3f78a5afe2e0ca903af905c2143
Erich Duda <dudaerich@gmail.com>
2015-07-20 16:16:46 +0200

    modified:   predani-dat/download.py

--- predani-dat/download.py
+++ predani-dat/download.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
 import cgi, cgitb
 import httplib, urllib
-import csv
+import csv, json
 import sys
 import HTMLParser
 import re
+import urllib2
 
-# cgitb.enable()
+cgitb.enable()
 
 ############ IDs of tables ######################
 tables = {
@@ -24,6 +24,29 @@ tables = {
   'zcm'    : ['1vdvc6hZoO8u3Gg3U8_O_JgIaRvE56rUHr_C5RmfR'],
 }
 #################################################
+
+class LabelData:
+    label_data = None
+
+    def _downloadData(self, institution):
+        url = "http://195.113.155.123/cgi-bin/addlabeltomap.py?action=getall&institution={}&token=cab08dc4-e7c6-4ca1-b2ad-393ec198c31d".format(institution)
+        response = json.load(urllib2.urlopen(url))
+
+        self.label_data = {}
+        if response.get("status", "") == "ok":
+            data = response.get("data", [])
+            for row in data:
+                key = row["key"]
+                value = row["value"]
+                self.label_data[key] = value
+
+
+    def getLabel(self, id, institution):
+        if self.label_data is None:
+            self._downloadData(institution)
+        return self.label_data.get(id, "")
+
+labelData = LabelData()
 
 def getNLabel(value):
     return "N" if value[0] == "0" else "S"
@@ -61,31 +84,47 @@ def convertToArchivesFormat(value):
   flag = "0" if degrees >= 0 else "1"
   return "%s%03d°%02d´%02d´´" % (flag, abs(degrees), minutes, seconds)
 
+def appendGeoData(header, data):
+    institution_index = header.index("institution")
+    data[institution_index] = re.sub(r"\d+$", "", data[institution_index])
+    south_west = data[header.index("south_west")]
+    north_east = data[header.index("north_east")]
+    if south_west and north_east:
+        south034,west034 = map(convertTo034Format, south_west.split(","))
+        north034,east034 = map(convertTo034Format, north_east.split(","))
+        south255,west255 = map(convertTo255Format, south_west.split(","))
+        north255,east255 = map(convertTo255Format, north_east.split(","))
+        southArchives,westArchives = map(convertToArchivesFormat, south_west.split(","))
+        northArchives,eastArchives = map(convertToArchivesFormat, north_east.split(","))
+        data.append("$$d%s%s$$e%s%s$$f%s%s$$g%s%s" % (getELabel(west034), getValue(west034), getELabel(east034), getValue(east034), getNLabel(north034), getValue(north034), getNLabel(south034), getValue(south034)))
+        data.append("$$c(%s %s--%s %s/%s %s--%s %s)" % (getELabel(west255), getValue(west255), getELabel(east255), getValue(east255), getNLabel(north255), getValue(north255), getNLabel(south255), getValue(south255)))
+        data.append("GPS: %s%s--%s%s/%s%s--%s%s" % (getValue(northArchives), getNLabel(northArchives), getValue(southArchives), getNLabel(southArchives), getValue(westArchives), getELabel(westArchives), getValue(eastArchives), getELabel(eastArchives)))
+    else:
+        data.append("")
+        data.append("")
+        data.append("")
+
+def appendLabelData(header, data):
+    institution_index = header.index("institution")
+    id_index = header.index("georeferencer_id");
+    institution = data[institution_index]
+    id = data[id_index]
+    data.append(labelData.getLabel(id, institution))
+
+
 
 def transformHeader(header):
-  header[header.index("collection")] = "institution"
-  header.append("marc034")
-  header.append("marc255")
-  header.append("archives")
+    header[header.index("collection")] = "institution"
+    header.append("marc034")
+    header.append("marc255")
+    header.append("archives")
+    header.append("label")
 
 def transformData(header, data):
-  institution_index = header.index("institution")
-  data[institution_index] = re.sub(r"\d+$", "", data[institution_index])
-  south_west = data[header.index("south_west")]
-  north_east = data[header.index("north_east")]
-  if south_west and north_east:
-    south034,west034 = map(convertTo034Format, south_west.split(","))
-    north034,east034 = map(convertTo034Format, north_east.split(","))
-    south255,west255 = map(convertTo255Format, south_west.split(","))
-    north255,east255 = map(convertTo255Format, north_east.split(","))
-    southArchives,westArchives = map(convertToArchivesFormat, south_west.split(","))
-    northArchives,eastArchives = map(convertToArchivesFormat, north_east.split(","))
-    data.append("$$d%s%s$$e%s%s$$f%s%s$$g%s%s" % (getELabel(west034), getValue(west034), getELabel(east034), getValue(east034), getNLabel(north034), getValue(north034), getNLabel(south034), getValue(south034)))
-    data.append("$$c(%s %s--%s %s/%s %s--%s %s)" % (getELabel(west255), getValue(west255), getELabel(east255), getValue(east255), getNLabel(north255), getValue(north255), getNLabel(south255), getValue(south255)))
-    data.append("GPS: %s%s--%s%s/%s%s--%s%s" % (getValue(northArchives), getNLabel(northArchives), getValue(southArchives), getNLabel(southArchives), getValue(westArchives), getELabel(westArchives), getValue(eastArchives), getELabel(eastArchives)))
-  else:
-    data.append("")
-    data.append("")
+    appendGeoData(header, data)
+    appendLabelData(header, data)
+
+
 
 def generateCSV(table, institution = None, printHeader = False):
   query = "SELECT * FROM {0}".format(table)
