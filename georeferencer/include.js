b7f79eb9b0dc682cd726d868d31619d15cdf9d6b
Erich Duda <dudaerich@gmail.com>
2015-08-10 12:49:56 +0200

    imagesearch developing

--- georeferencer/include.js
+++ georeferencer/include.js
@@ -38,14 +38,14 @@ if (activeItem.innerHTML.trim() == 'Umísti') {
   loadScript('http://staremapy.cz/georeferencer/umisti/main-compiled.js', function() {
     georeferencer.umisti.main();
   });
-  loadScript('http://staremapy.cz/georeferencer/imagesearch/imagesearch-compiled.js', function() {
+  /*loadScript('http://staremapy.cz/georeferencer/imagesearch/imagesearch-compiled.js', function() {
     loadCss('http://staremapy.cz/georeferencer/imagesearch/css/main.css');
     loadCss('http://staremapy.cz/georeferencer/imagesearch/css/button.css');
     loadCss('http://staremapy.cz/georeferencer/imagesearch/css/css3button.css');
     loadCss('http://staremapy.cz/georeferencer/imagesearch/css/dialog.css');
     loadCss('http://staremapy.cz/georeferencer/imagesearch/css/checkbox.css');
     georeferencer.imagesearch.main();
-  });
+  });*/
 } else if (activeItem.innerHTML.trim() == 'Analyzuj') {
   loadScript('http://staremapy.cz/georeferencer/analyzuj.js');
 }
