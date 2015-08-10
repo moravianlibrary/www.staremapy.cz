a1afc21cdec97e5b6f863a839125f229d7fdc74a
Erich Duda <dudaerich@gmail.com>
2015-08-10 14:00:08 +0200

    imagesearch developing

--- georeferencer/umisti/src/main-imagesearch.js
+++ georeferencer/umisti/src/main-imagesearch.js
@@ -23,8 +23,8 @@ georeferencer.imagesearch.main = function() {
       var xhr = e.target;
       var json = xhr.getResponseJson();
       var dialog = new georeferencer.imagesearch.Dialog(json['data']);
-      dialog.getElement().id = 'imagesearch-dialog';
       dialog.setVisible(true);
+      dialog.getElement().id = 'imagesearch-dialog';
       georeferencer.imagesearch.hideLoading();
     });
   });
